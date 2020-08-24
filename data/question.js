1403f6ae12bb91348fb804e24f42834f05cc7869
thnamm <nam.tran@journeyh.io>
2020-08-24 09:26:05 +0700

    Import data (list of item question)

---
+++ data/question.js
@@ -0,0 +1,31 @@
+const question = [{
+    id: 0,
+    question: "What is your favourite anime? Tell us why you love it, otherwise why you don't?"
+}, {
+    id: 1,
+    question: "Summarize Marvel universe in 300 words!"
+}, {
+    id: 2,
+    question: "Both dogs and cats are adorable pet, which of them you love the most? And why?"
+}, {
+    id: 3,
+    question: "What do you expect from our company?"
+}, {
+    id: 4,
+    question: "Give us 3 main problems you experienced in working with teammates!"
+}, {
+    id: 5,
+    question: "If you have 1 billion dollars, what will be your future plan?"
+}, {
+    id: 6,
+    question: "Difference of Western and Eastern people?"
+}, {
+    id: 7,
+    question: "Write a letter to your leader for being absent for a week!"
+}, {
+    id: 8,
+    question: "What type of work environment do you prefer?"
+}, {
+    id: 9,
+    question: "Was the bigbang just an accident?"
+}]
